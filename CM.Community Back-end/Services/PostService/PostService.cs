f92905ea69dedc39a03fd80db47f3ab76177a61a
AfroNeko <lisatyem@gmail.com>
2022-12-15 14:47:31 +0100

    Added service folder

---
+++ CM.Community Back-end/Services/PostService/PostService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CM.Community_Back_end.Services.PostService
+{
+    public interface PostService
+    {
+        
+    }
+}
