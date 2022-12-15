39f8e4e2a222d5f166af895d7651f9dfc99ec32d
AfroNeko <lisatyem@gmail.com>
2022-12-15 14:37:37 +0100

    Added add post functionallity

--- CM.Community Back-end/Controllers/PostController.cs
+++ CM.Community Back-end/Controllers/PostController.cs
@@ -13,17 +13,36 @@ namespace CM.Community_Back_end.Controllers
     {
         private static List<Post> posts = new List<Post>{
             new Post{
+                postID = 1,
                 postTitle = "I'm the first post",
-                postText = "This post is the first post of the page! Exciting right?"
+                postText = "This post is the first post of the page! Exciting right?",
+                publicationDate = new DateTime(),
+                Subject = "Subject one"
             },
             new Post{
+                postID = 2,
                 postTitle = "I'm the second post",
-                postText = "To top it off, we've got not one, but two posts!!"}
+                postText = "To top it off, we've got not one, but two posts!!",
+                publicationDate = new DateTime(),
+                Subject = "Subject two"}
         };
 
-        [HttpGet]
+        [HttpGet("GetAll")]
         public ActionResult<List<Post>> get()
         {
+            return Ok(posts);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Post> GetSingle(int id)
+        {
+            return Ok(posts.FirstOrDefault(p => p.postID == id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Post>> AddPost(Post newPost)
+        {
+            posts.Add(newPost);
             return Ok(posts);
         }
 
