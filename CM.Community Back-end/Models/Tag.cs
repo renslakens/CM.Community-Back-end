b3ba7ced364eea3cf3b3be205cd46bb48541e7c4
Bramgelten <bramgelten@gmail.com>
2022-12-20 15:05:16 +0100

    added auto increment to comment, group, post, tag and user table id's

--- CM.Community Back-end/Models/Tag.cs
+++ CM.Community Back-end/Models/Tag.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace CM.Community_Back_end.Models
 {
     public class Tag
     {
-        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity), Key()]
         public int tagID { get; set; }
 
         [StringLength(25)]
