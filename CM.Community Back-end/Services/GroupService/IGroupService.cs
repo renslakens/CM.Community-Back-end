405dfd85e677d59a1e9482093d02870b29921074
Bramgelten <bramgelten@gmail.com>
2022-12-20 14:52:25 +0100

    fixed joinging usergroup

--- CM.Community Back-end/Services/GroupService/IGroupService.cs
+++ CM.Community Back-end/Services/GroupService/IGroupService.cs
@@ -9,6 +9,7 @@ namespace CM.Community_Back_end.Services.GroupService
         Task<List<Group>> addGroup(Group newGroup);
         Task<List<Group>> updateGroup(Group updatedGroup);
         Task<List<Group>> deleteGroup(Group deletedGroup);
-        Task<List<UserGroup>> joinGroup(Group newUserInGroup);
+        Task<List<UserGroup>> joinGroup(UserGroup newUserInGroup);
+        //Task<object?> joinGroup(UserGroup newUserInGroup);
     }
 }
--- CM.Community Back-end/Services/UserService/UserService.cs
+++ CM.Community Back-end/Services/UserService/UserService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -82,9 +83,19 @@ namespace CM.Community_Back_end.Services.UserService
                 new Claim(ClaimTypes.Name, user.userFirstName)
             };
 
-            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes())
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
 
-            return string.Empty;
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
