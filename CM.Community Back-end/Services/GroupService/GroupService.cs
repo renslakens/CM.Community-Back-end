d4065b2a98b3126b0b20d52e6b58634fc45354b0
Bramgelten <bramgelten@gmail.com>
2022-12-22 14:42:34 +0100

    started on deleting a group

--- CM.Community Back-end/Services/GroupService/GroupService.cs
+++ CM.Community Back-end/Services/GroupService/GroupService.cs
@@ -39,8 +39,20 @@ namespace CM.Community_Back_end.Services.GroupService
             throw new NotImplementedException();
         }
 
-        public Task<Group> deleteGroup(Group deletedGroup) {
-            throw new NotImplementedException();
+        public String deleteGroup(Group deletedGroup) {
+            //throw new NotImplementedException();
+            var testing = _context;
+            var itemToRemove = _context.Groups.SingleOrDefault(x => x.groupID == deletedGroup.groupID);
+
+            var returntext = "No group with this ID";
+
+            if (itemToRemove != null)
+            {
+                _context.Groups.Remove(itemToRemove);
+                _context.SaveChanges();
+                returntext = "Group with this id has been deleted";
+            }
+            return returntext;
         }
 
         public async Task<List<Group>> GetAllGroups()
@@ -75,12 +87,16 @@ namespace CM.Community_Back_end.Services.GroupService
         {
             var testing = _context;
             var groups = testing.Groups.FromSql($"Select G.groupID, G.Groupname From Groups G INNER JOIN UserGroups UG ON G.groupID = UG.GroupID WHERE UserId = {currentUserID}").ToList<Group>();
-            //var groups = testing.Groups.FromSql($"Select * FROM UserGroups WHERE UserId = {currentUserID}").ToList<Group>();
-            //var groups1 = testing.Groups.FromSql($"Select * FROM UserGroups WHERE UserId = {currentUserID}").ToList<Group>();
             return groups;
+        }
 
-            //var innerJoinQuery =
-            //from filteredgroups in User.
+        public async Task<List<Group>> getUnjoinedGroupsByUserID(/*int currentUserID*/)
+        {
+            var testing = _context;
+            var createView = testing.Groups.FromSql($"SELECT G.groupName, G.groupID FROM Groups G WHERE G.groupID IN (SELECT G.groupID FROM UserGroups UG WHERE UG.groupID = G.groupID HAVING COUNT(UG.userID) <= 1);").ToList<Group>;
+            //var dropView = testing.Groups.FromSql($" DROP VIEW IF EXISTS notjoinedgroups;");
+            //var groups = createView.Groups.FromSql($"SELECT * FROM notjoinedgroups WHERE groupID != ANY(SELECT groupID FROM UserGroups WHERE userID = {currentUserID});").ToList<Group>();
+            return createView;
         }
     }
 }
--- CM.Community Back-end/Services/GroupService/IGroupService.cs
+++ CM.Community Back-end/Services/GroupService/IGroupService.cs
@@ -8,7 +8,7 @@ namespace CM.Community_Back_end.Services.GroupService
         Task<Group> getGroupById(int Id);
         Task<Group> addGroup(Group newGroup);
         Task<Group> updateGroup(Group updatedGroup);
-        Task<Group> deleteGroup(Group deletedGroup);
+        String deleteGroup(Group deletedGroup);
         Task<UserGroup> joinGroup(UserGroup newUserInGroup);
         Task<List<Group>> getGroupByUserID(int currentUserID);
     }
