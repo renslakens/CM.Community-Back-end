839674558e11e4209b08f15d8b92a57e5b32343f
Anika <anika.w.01@gmail.com>
2023-01-18 14:19:18 +0100

    Tests SignUpUser and LoginUser pass

--- CM.Community Back-end/Tests/UserServiceTests.cs
+++ CM.Community Back-end/Tests/UserServiceTests.cs
@@ -5,36 +5,29 @@ using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 using System.Linq.Expressions;
+using CM.Community_Back_end.DTO;
+using System;
 
 namespace CM.Community_Back_end.Tests
 {
     public class UserServiceTests
     {
         [Fact]
-        public void NewUserIsAdded()
+        public void SignUpUser()
         {
             //arrange
             var User = new List<User>
             {
-                new User {
-                UserId = 1,
-                userEmail = "",
-                userFirstName = "",
-                userLastName = "",
-                userBirthDate = DateTime.Now,
-                userPassword = ""
-                }
-
             }.AsQueryable();
 
             var AddUser = new User
             {
-                UserId = 2,
-                userEmail = "",
-                userFirstName = "",
-                userLastName = "",
+                UserId = 1,
+                userEmail = "testing@email.com",
+                userFirstName = "TestFirstName",
+                userLastName = "TestLastName",
                 userBirthDate = new DateTime(2000, 01, 01),
-                userPassword = ""
+                userPassword = "P@ssw0rd"
             };
 
             var mockSetUser = new Mock<DbSet<User>>(); //tabel
@@ -58,6 +51,54 @@ namespace CM.Community_Back_end.Tests
 
             //assert
             Assert.NotNull(result);
+            Assert.Equal(result.Result, AddUser);
+        }
+
+        [Fact]
+        public void LoginUser()
+        {
+            //arrange
+            var User = new List<User>
+            {
+                new User {
+                    UserId = 1,
+                    userEmail = "testing@email.com",
+                    userFirstName = "TestFirstName",
+                    userLastName = "TestLastName",
+                    userBirthDate = new DateTime(2000, 01, 01),
+                    userPassword = "P@ssw0rd"
+                }
+            }.AsQueryable();
+
+            var LoginUser = new UserDTO
+            {
+                userEmail = "testingsecond@email.com",
+                userPassword = "P@ssw0rd"
+            };
+
+            var mockSetUser = new Mock<DbSet<User>>(); //tabel
+
+            // Mock packet tabel via IQueryable zie microsoft documentatie: https://learn.microsoft.com/en-us/ef/ef6/fundamentals/testing/mocking
+            // Seed
+            mockSetUser.As<IQueryable<User>>().Setup(m => m.Provider).Returns(User.Provider);
+            mockSetUser.As<IQueryable<User>>().Setup(m => m.Expression).Returns(User.Expression);
+            mockSetUser.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(User.ElementType);
+            mockSetUser.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(() => User.GetEnumerator());
+
+            var mockContext = new Mock<ApplicationDbContext>();//database
+            mockContext.Setup(m => m.Users).Returns(mockSetUser.Object);
+
+            var configuration = new Mock<IConfiguration>();//configuration
+
+            var userService = new UserService(configuration.Object, mockContext.Object);
+
+            //act
+            var result = userService.loginUser(LoginUser);
+
+            //assert
+            Assert.NotNull(result);
+            Assert.IsType<Task<String>>(result);
+
         }
     }
 }
